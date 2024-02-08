e8d052616fbad61d2148668878a9875f21f54b89
Pierluigi De Meo <74504067+Pierludm2002@users.noreply.github.com>
2024-02-08 16:35:18 +0100

    BusinessLogic.java funzionante solo per assegnazione idutente

--- src/main/java/Controller/BusinessLogic.java
+++ src/main/java/Controller/BusinessLogic.java
@@ -1,16 +1,58 @@
 package Controller;
+//
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Database.DBconnection;
 
 public class BusinessLogic {
-	int contatoreUtenti = 2; 
+	//
+	Connection conn; 
+	
+	int contatoreUtenti = 0; 
 	int contatoreFamiglia = 0; 
 	int contatoreTransazioni = 0; 
-	
+	//
+	BusinessLogic(){ 
+		try { 
+			conn = DBconnection.getInstance().getConnection(); 
+		}catch(SQLException e) { 
+			System.out.println("Connessione per ottenere numero di utenti fallita!");
+			e.printStackTrace();
+			} 
+	}
+	public int SetNumUtenti(int contatoreUtenti){
+		try { 
+			Statement stm = conn.createStatement(); 
+			String query = "select count(*) as numUtenti from utente "; 
+			ResultSet rs = stm.executeQuery(query);
+			if(rs.next()) { 
+				contatoreUtenti = rs.getInt("numUtenti"); 
+			}
+			}catch(SQLException e){ 
+			 	System.out.println("Errore durante la richiesta : " + e.getMessage());
+			 	e.printStackTrace();
+			}finally{
+				if(conn!=null){ 
+					try{
+						conn.close(); 	
+					}catch(SQLException e){ 
+						System.out.println("Errore durante la chiusura della connessione col Database:");
+					e.printStackTrace();  
+			}
+		}
+	}
+	return contatoreUtenti++; 
+}
 	
 	public String generaId(TipoEntita entita) {
+		
+	
 		switch(entita) { 
 			case User: 
-				contatoreUtenti++; 
-				return generaId("u", contatoreUtenti);
+				return generaId("u", SetNumUtenti());
 			case Famiglia: 
 				contatoreFamiglia++; 
 				return generaId("f", contatoreFamiglia); 
